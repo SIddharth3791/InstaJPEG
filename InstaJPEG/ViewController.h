9cfb46e85490b97f22edba1bffc75e36ced9aa07
siddharth <patelsi14@students.ecu.edu>
2017-02-08 02:45:35 -0500

    Initial Commit

---
+++ InstaJPEG/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InstaJPEG
+//
+//  Created by Siddharth Patel on 2/8/17.
+//  Copyright © 2017 Siddharth Patel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
